1bcbddc5eb495233b7117ca8654d7fd133b66b82
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-08-21 16:46:32 -0300

    eliti projeto

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-SESSION_start();
-$_SESSION["codigo_usuario"] = $query[1];
-$_SESSION["nome_usuario"] = $query[2];
-$_SESSION["permissao"] = $query[3];
-$_SESSION["email"] = $query[4];
-$_SESSION["Senha"] = $query[5];
-    
+
+MODULE_NOT_FOUND
+
+{
+    "scripts": {
+      "build-project": "node build-project.js"
+    }
+  }
